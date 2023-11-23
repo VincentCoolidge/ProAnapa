b2304c929d3acb24dc8f43f7cc6383f0fe524d16
Anatoly <anatolysavchenko95@gmail.com>
2023-11-24 00:35:09 +0300

    update

--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -1,7 +1,40 @@
+import { dataOurServices } from "./mockData";
+
+import "./styled.css";
+
 const OurServices = () => {
   return (
-    <div>
+    <div className="root_ourServices">
       <h3 className="heading_3">Наши услуги</h3>
+      <div className="grid_ourServices">
+        {dataOurServices.map(({ id, title, desc }) => {
+          if (id === 0) {
+            return (
+              <div
+                className="box_info_first_our_service"
+                key={`our-services-${id}`}
+              >
+                <p className="text-our_services">
+                  {title}
+                  <div className="pre-our_services" />
+                  {desc}
+                </p>
+              </div>
+            );
+          }
+
+          return (
+            <div className="box_our_service" key={`our-services-${id}`}>
+              <div className={`box_our_service_background_img_${id}`}></div>
+              <div className="box_our_service_box">
+                <div className="box_our_service_title">{title}</div>
+                <div className="box_our_service_desc">{desc}</div>
+                
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
